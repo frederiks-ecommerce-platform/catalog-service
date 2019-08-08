b99a6067d8f33d6b76f3ee7247ea41865a33790c
frederik.steffen <frederik.steffen@gmail.com>
2019-08-08 14:38:55 +0200

    initial commit

---
+++ api/src/test/java/org/samply/catalog/api/NativeItemResourceIT.java
@@ -0,0 +1,9 @@
+package org.samply.catalog.api;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeItemResourceIT extends ItemResourceTest {
+
+    // Execute the same tests but in native mode.
+}
