1456c78772668ab7d206dd24dd5d337bb3f7dd6a
frederik.steffen <frederik.steffen@gmail.com>
2019-09-29 18:08:11 +0200

    made user-id-header param optional for api docs

--- api/src/main/java/org/samply/catalog/api/web/OASConfigFilter.java
+++ api/src/main/java/org/samply/catalog/api/web/OASConfigFilter.java
@@ -4,6 +4,7 @@ import java.util.Optional;
 import org.eclipse.microprofile.config.Config;
 import org.eclipse.microprofile.config.ConfigProvider;
 import org.eclipse.microprofile.openapi.OASFilter;
+import org.eclipse.microprofile.openapi.models.parameters.Parameter;
 import org.eclipse.microprofile.openapi.models.security.SecurityScheme;
 
 public class OASConfigFilter implements OASFilter {
@@ -19,6 +20,22 @@ public class OASConfigFilter implements OASFilter {
         tokenUrl.ifPresent(securityScheme.getFlows().getPassword()::setTokenUrl);
 
         return securityScheme;
+    }
+
+    @Override
+    public Parameter filterParameter(Parameter parameter) {
+        Config config = ConfigProvider.getConfig();
+
+        boolean disableUserIdHeader = config.getValue(
+                "catalog-api.disable-user-id-header",
+                Boolean.class
+        );
+
+        if (disableUserIdHeader && "X-User-Id".equals(parameter.getName())) {
+            return null;
+        }
+
+        return parameter;
     }
 
 }
